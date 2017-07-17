fe47bea50c12107734c46e05b2e809537908c3c9
clarkgh <demolisho@hotmail.com>
2017-07-17 15:51:04 -0700

    github contributer update + serving page

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const express = require('express'),
   app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
 app.listen(3000, function() {
   console.log("listening on port 3000");
 });
