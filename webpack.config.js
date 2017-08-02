86511f4c78992bbfb71c4614979d8a049db6385a
clarkgh <demolisho@hotmail.com>
2017-08-02 16:26:25 -0700

    set up webpack/babel, error with the directories, simple fix. Taking a break now.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path'),
+  webpack = require('webpack');
+
+module.exports = {
+  entry: './js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
+    }]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+};
