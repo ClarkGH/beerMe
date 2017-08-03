5bcb162c8b25583f43fb388860567c176ecaf231
clarkgh <demolisho@hotmail.com>
2017-08-02 17:23:39 -0700

    webpack and babel functioning properly

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path'),
   webpack = require('webpack');
 
 module.exports = {
-  entry: './js/main.js',
+  entry: '.public/js/main.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'main.bundle.js'
